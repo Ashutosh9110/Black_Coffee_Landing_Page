daf43c7e391ecba3100f0504f62fe91e852e3acb
Ashutosh9110 <ashusingh19911082@gmail.com>
2025-02-12 13:52:00 +0530

    added animation for Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,8 @@
 // import BgImage from "../../assets/bg-slate.png"
 import BgImage from "../../assets/bg-slate.png?url";
 import BlackCoffee from "../../assets/black.png?url";
-
+import Navbar from "../Navbar/Navbar";
+import { motion } from "framer-motion";
 
 
 // const bgImage = {
@@ -19,28 +20,86 @@ const Hero = () => {
     <section className="min-h-[750px] w-full">
       <div className="container">
         {/* Navbar Section */}
+
+        <Navbar />
+
         {/* Hero Section */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]">
         {/* text content section */}
         <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
-          <h1 className="text-7xl font-bold leading-tight ml-14">Your Black Coffee</h1>
-        <div className="relative">
+          <motion.h1 
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 10,
+            delay: 1
+          }}
+          className="text-7xl font-bold leading-tight ml-14">Your Black Coffee</motion.h1>
+          <motion.div 
+             initial={{ opacity: 0, y: -100 }}
+             animate={{ opacity: 1, y: 0 }}
+             transition={{
+               type: "spring",
+               stiffness: 100,
+               damping: 10,
+               delay: 1.2
+             }}
+               className="relative">
           <div className="relative z-10 space-y-4">
-            <h1 className="text-2xl">Black Lifestyle Lovers</h1>
-            <h1 className="text-sm opacity-55 leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati provident at tempora fugiat accusantium quibusdam impedit, incidunt enim voluptatum.</h1>
+              <h1 className="text-2xl">Black Lifestyle Lovers</h1>
+              <h1 className="text-sm opacity-55 leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati provident at tempora fugiat accusantium quibusdam impedit, incidunt enim voluptatum.</h1>
           </div>
           <div className="absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25"></div>
-        </div>
+        </motion.div>
         </div>
         {/* Hero image section */}
         <div className="relative">
-          <img src={BlackCoffee} 
+          <motion.img 
+           initial={{ opacity: 0, scale: 0 }}
+           animate={{ opacity: 1, scale: 1 }}
+           transition={{
+             type: "spring",
+             stiffness: 100,
+             damping: 10,
+             delay: 0.4
+           }}
+          src={BlackCoffee} 
           alt=""
           className="relative z-40 h-[400px] md:h-[700px] img-shadow"
           />
+          {/* orange cirle ring */}
+          <div className="h-[180px] w-[180px] absolute top-24 -right-16 border-primary border-[20px] rounded-full z-10"></div>
 
+          {/* Big Text Section */}
+
+          <div className="absolute -top-2 left-[200px] z-[1]">
+          <h1 className="text-[140px] scale-150 font-bold text-darkGray/40 leading-none">
+            Your Black Coffee
+          </h1>
+          </div>
         </div>
         {/* third div section */}
+        <motion.div 
+           initial={{ opacity: 0, y: -100 }}
+           animate={{ opacity: 1, y: 0 }}
+           transition={{
+             type: "spring",
+             stiffness: 100,
+             damping: 10,
+             delay: 1.2
+           }}
+            className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
+          <h1 className="opacity-0 text-7xl font-bold leading-tight ml-14">Your Black Coffee</h1>
+        <div className="relative">
+          <div className="relative z-10 space-y-4">
+            <h1 className="text-2xl">Dark Coffee </h1>
+            <h1 className="text-sm opacity-55 leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati provident at tempora fugiat accusantium   voluptatum tempora fugiat voluptatum.</h1>
+          </div>
+          <div className="absolute -top-6 -right-10 w-[250px] h-[190px] bg-darkGray/50"></div>
+        </div>
+        </motion.div>
         <div></div>
         </div>
       </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <nav className="absolute top-0 left-0 w-full pt-10 text-white z-20">
+    <div className="container">
+      <div>
+        {/* Logo section */}
+        <h1 className="text-2xl font-semibold">
+          <span className="text-primary">Black </span>Coffee
+        </h1>
+        {/* Hamburger Menu Section */}
+        <div>
+          <GiHamburgerMenu className="text-3xl cursor-pointer" />
+        </div>
+      </div>
+    </div>
+  </nav>;
 };
 
 export default Navbar;
